7926f3882f6dfc0c34082b6f3d9419626d896153
BTH <BTH@H473-0015>
2017-01-30 16:16:05 +0100

    Created swapchain

--- testbench-master/gl_testbench/VulkanRenderer.cpp
+++ testbench-master/gl_testbench/VulkanRenderer.cpp
@@ -1,6 +1,10 @@
 #include "VulkanRenderer.h"
 #include <ConsoleThread.h>
+#include <array>
 #include <SDL_syswm.h>
+#include <algorithm>
+#undef max
+#undef min
 
 
 VulkanRenderer::VulkanRenderer()
@@ -101,15 +105,15 @@ int VulkanRenderer::initialize(unsigned int width, unsigned int height)
 		"Frengine",
 		VK_MAKE_VERSION(1, 0, 0)
 	);
-	const char* extensions[] = { "VK_KHR_surface", "VK_KHR_win32_surface" };
+	std::array<const char*, 2> extensions = { "VK_KHR_surface", "VK_KHR_win32_surface"};
 	const auto vkInstCreateInfo = VulkanHelpers::MakeInstanceCreateInfo(
 		0,
 		vkAppInfo,
 		validationLayers.size(),
 		validationLayers.data(),
 		nullptr,
-		2,
-		extensions
+		extensions.size(),
+		extensions.data()
 	);
 
 
@@ -135,10 +139,17 @@ int VulkanRenderer::initialize(unsigned int width, unsigned int height)
 	requiredFeatures.multiDrawIndirect = supportedFeatures.multiDrawIndirect;
 
 	const auto deviceQueueCreateInfo = &VulkanHelpers::MakeDeviceQueueCreateInfo(0, 1);
-
+	std::array<const char*, 1> deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
 	const auto deviceCreateInfo = &VulkanHelpers::MakeDeviceCreateInfo(
 		1,
-		deviceQueueCreateInfo
+		deviceQueueCreateInfo,
+		0,
+		nullptr,
+		nullptr,
+		nullptr,
+		deviceExtensions.size(),
+		deviceExtensions.data(),
+		0
 	);
 
 
@@ -188,11 +199,83 @@ int VulkanRenderer::initialize(unsigned int width, unsigned int height)
 	wndCreateInfo.hinstance = wc.hInstance;
 	wndCreateInfo.flags = 0;
 	wndCreateInfo.pNext = nullptr;
-	auto CreateWin32SurfaceKHR = (PFN_vkCreateWin32SurfaceKHR) vkGetInstanceProcAddr(_vkInstance, "vkCreateWin32SurfaceKHR");
-
-	if (!CreateWin32SurfaceKHR || vkCreateWin32SurfaceKHR(_vkInstance, &wndCreateInfo, nullptr, &_vkSurface) != VK_SUCCESS)
+	
+	if (vkCreateWin32SurfaceKHR(_vkInstance, &wndCreateInfo, nullptr, &_vkSurface) != VK_SUCCESS)
 		throw std::runtime_error("Window surface creation failed.");
 	
+	/************** Set up swap chain ****************/
+	VkSurfaceCapabilitiesKHR capabilities;
+	std::vector<VkSurfaceFormatKHR> supportedFormats;
+	std::vector<VkPresentModeKHR> supportedPresentModes;
+
+	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(_vkPhysicalDevices[0], _vkSurface, &capabilities);
+	
+	uint32_t formatCount;
+	vkGetPhysicalDeviceSurfaceFormatsKHR(_vkPhysicalDevices[0], _vkSurface, &formatCount, nullptr);
+	supportedFormats.resize(formatCount);
+	vkGetPhysicalDeviceSurfaceFormatsKHR(_vkPhysicalDevices[0], _vkSurface, &formatCount, supportedFormats.data());
+
+	uint32_t presentModeCount;
+	vkGetPhysicalDeviceSurfacePresentModesKHR(_vkPhysicalDevices[0], _vkSurface, &presentModeCount, nullptr);
+	supportedPresentModes.resize(presentModeCount);
+	vkGetPhysicalDeviceSurfacePresentModesKHR(_vkPhysicalDevices[0], _vkSurface, &presentModeCount, supportedPresentModes.data());
+
+	VkSurfaceFormatKHR bestFormat;
+	if (supportedFormats.size() == 1 && supportedFormats[0].format == VK_FORMAT_UNDEFINED)
+	{
+		bestFormat = supportedFormats[0];
+	}
+	else
+	{
+		//Settle for first format unless something better comes along
+		bestFormat = supportedFormats[0];
+		for (const auto& i : supportedFormats)
+		{
+			if (i.format == VK_FORMAT_B8G8R8A8_UNORM && i.format == VK_COLORSPACE_SRGB_NONLINEAR_KHR)
+			{
+				bestFormat = i;
+				break;
+			}
+		}
+	}
+	//Unless something better comes along, use the standard mode
+	VkPresentModeKHR bestPresentMode = VK_PRESENT_MODE_FIFO_KHR;
+	for (const auto& i : supportedPresentModes)
+	{
+		if (i == VK_PRESENT_MODE_MAILBOX_KHR)
+			bestPresentMode = i;
+	}
+
+	VkExtent2D bestExtent = { width, height };
+	bestExtent.width = std::max(capabilities.minImageExtent.width, std::min(capabilities.maxImageExtent.width, width));
+	bestExtent.height = std::max(capabilities.minImageExtent.height, std::min(capabilities.maxImageExtent.height, height));
+
+	uint32_t imageCount = std::min(capabilities.minImageCount + 1, capabilities.maxImageCount);
+
+	VkSwapchainCreateInfoKHR swapCreateInfo;
+	ZeroMemory(&swapCreateInfo, sizeof(swapCreateInfo));
+	swapCreateInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+	swapCreateInfo.surface = _vkSurface;
+	swapCreateInfo.minImageCount = imageCount;
+	swapCreateInfo.imageFormat = bestFormat.format;
+	swapCreateInfo.imageColorSpace = bestFormat.colorSpace;
+	swapCreateInfo.imageExtent = bestExtent;
+	swapCreateInfo.imageArrayLayers = 1;
+	swapCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+	swapCreateInfo.preTransform = capabilities.currentTransform;
+	swapCreateInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+	swapCreateInfo.presentMode = bestPresentMode;
+	swapCreateInfo.clipped = VK_TRUE;
+	swapCreateInfo.oldSwapchain = VK_NULL_HANDLE;
+	//Assume graphics family is the same as present family
+	swapCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	swapCreateInfo.queueFamilyIndexCount = 0;
+	swapCreateInfo.pQueueFamilyIndices = nullptr;
+	
+	if (vkCreateSwapchainKHR(_vkDevice, &swapCreateInfo, nullptr, &_vkSwapChain) != VK_SUCCESS)
+		throw std::runtime_error("Failed to create swapchain");
+
+
 
 	return 0;
 }
